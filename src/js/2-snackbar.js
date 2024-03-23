37ad0976898a4c78b54668498690cd4153816c71
Hanna-Nagrehelna <anna.nagrebelna@gmail.com>
2024-03-23 20:30:37 +0200

    task-2 done with one problem with library iziToast

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,58 @@
+// Завдання 2 - Генератор промісів
+
+const form = document.querySelector(".form");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    
+    // Отримання значення поля затримки при кожному відправленні форми
+    const delayInput = parseInt(document.querySelector("input[name='delay']").value);
+    // Отримання значення поля стану при кожному відправленні форми
+    const stateInput = document.querySelector("input[name='state']:checked");
+    
+    const delay = parseInt(delayInput);
+
+    const promise = new Promise((resolve, reject) => {
+        if (stateInput && stateInput.value === "fulfilled") {
+            setTimeout(() => {
+                resolve(delay);
+            }, delay);
+        } else {
+            setTimeout(() => {
+                reject(delay);
+            }, delay); // Вказуємо setTimeout для відтермінування
+        }
+    });
+
+    
+    promise
+        .then((delay) => {
+            console.log(`✅ Fulfilled promise in ${delay} ms`);
+        }).catch((delay) => {
+            console.log(`❌ Rejected promise in ${delay} ms`)
+        });
+
+    
+    /*    
+    promise
+            .then((delay) => {
+                iziToast.success({
+                    title: "✅ Fulfilled promise",
+                    message: `in ${delay} ms`,
+                    position: "topRight",
+                });
+            })
+            .catch((delay) => {
+                iziToast.error({
+                    title: "❌ Rejected promise",
+                    message: `in ${delay} ms`,
+                    position: "topRight",
+                });
+            });
+    */
+
+});
+
+
+
+
